a66805795c6de1fe269fbf12a043087ed0a0f338
Florian Cargoët <florian.cargoet@gmail.com>
2019-10-28 18:19:23 +0100

    Build logs

--- build.js
+++ build.js
@@ -4,21 +4,29 @@ const { execSync } = require("child_process");
 const BOARD_FQBN = "adafruit:samd:adafruit_feather_m0";
 
 // 1 - Find board
+console.log("👀 Scanning for boards…");
 const ports = JSON.parse(execSync("arduino-cli board list --format json"));
 const port = ports.find(p => p.boards && p.boards.find(b => b.FQBN === BOARD_FQBN));
 
 if (!port) {
-    console.error("Board not found");
+    console.error("❌ Board not found");
     process.exit(1);
+}
+else {
+    console.log(`✅ Board found: ${port.boards[0].name} on ${port.address}`);
 }
 
 // 2 - Compile, keep stdout in terminal
+console.log("🚧 Compiling…");
 execSync(`arduino-cli compile --fqbn ${BOARD_FQBN} .`, { stdio: "inherit" });
 
 // 3 - Kill current serial connection
 try {
+    console.log("✂️ Closing previous serial connection…");
     execSync("screen -XS arduino kill", { stdio: "inherit" });
 } catch (e) {}
 
 // 4 - Upload, keep stdout in terminal
+
+console.log("🚀 Uploading…");
 execSync(`arduino-cli upload -p ${port.address} --fqbn ${BOARD_FQBN} .`, { stdio: "inherit" });
